74c602f2849416fad8fe3e567023b564e118af30
jonesguidini <jonesguidini@gmail.com>
2017-06-09 18:04:54 -0300

    editing portfolio

--- js/geral.js
+++ js/geral.js
@@ -1,5 +1,28 @@
 $( document ).ready(function() {
 
+  $(".btn-listInfo-port").on('click', function(){
+
+      /*
+      $( ".list-collapse" ).each(function( index ) {
+        if(!$(this).hasClass("hide")){
+          $(this).addClass("hide");
+        }
+      });
+        */
+
+      var _object = $(this).next(".list-collapse");
+      var checkHidden = _object.is( ":hidden" );
+
+      if(checkHidden){ //if it is true
+        _object.removeClass("hide");
+        //_object.slideDown( "slow" );
+        //console.log("--->" + checkHidden);
+      }else {
+        //_object.slideUp( "slow" );
+        _object.addClass("hide");
+      }
+
+  });
 
   $("#bt_toggle").click(function(){
     $(".list-menu-celular-down").slideToggle("fast");
